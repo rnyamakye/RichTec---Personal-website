07b84c66f8eddc5f9b48bd57f572f3117cfbb78a
unknown <rnyamekye32@gmail.com>
2025-08-05 10:32:58 +0000

    Optimized images

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import { Navbar } from "./Components/Navbar";
 import "./App.css";
 import Home from "./Components/Home";
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route index path="/" element={<Home />} />
       </Routes>
+      <SpeedInsights />
+      <Analytics />
     </BrowserRouter>
   );
 }
